e8b4ea43ebbc99b8b9683117180cc948e362b604
PMuiruri <peninah.m.muiruri@gmail.com>
2018-12-05 00:12:28 +0200

    fixed bugs and updated the index and sqlstatements files

--- index.js
+++ index.js
@@ -15,6 +15,29 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+const homeworkStorage = require('./homeworkDb')(debug);
+const statushandling =[sendErrorPage, sendStatusPage];
+
+const getroutes = require('./routes/getroute')(homeworkStorage,...statushandling);
+const insertroutes = require('./routes/insertroute')(homeworkStorage,...statushandling);
+const deleteroutes = require('./routes/deleteroute')(homeworkStorage,...statushandling);
+const updatesroutes = require('./routes/updateroutes')(homeworkStorage,...statushandling);
+
+app.use('/', getroutes);
+app.use('/', insertroutes);
+app.use('/', deleteroutes);
+app.use('/', updatesroutes);
+
+app.get('/', (req,res) => res.sendFile(path.join(__dirname,'index.html')));
+
 server.listen(port, host, () =>
 	console.log(`Sever ${host} is serving at port ${port}`)
 );
+
+function sendErrorPage(res, message='Error', title='Error', header='Error') {
+	sendStatusPage(res, message, title,header);
+}
+
+function sendStatusPage(res, message='Status', title='Status', header='Status'){
+	return res.render('statusPage',{title:title,header:header,message:message});
+}
