431a1b14a6cf164d0ab3052407ec0d50c26f18d6
PMuiruri <peninah.m.muiruri@gmail.com>
2019-01-14 01:31:01 +0200

    created fixed sidebar

---
+++ h-project/src/components/sidebar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './sidebar.css';
+import PageImage from './image';
+
+class Sidebar extends Component {
+
+	render() {
+		return (
+			<div className="sidebar">
+				<PageImage imageURL={this.props.imageURL} />
+			</div>
+		);
+	}
+}
+
+export default Sidebar;
