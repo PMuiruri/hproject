496ccaccf1de6a342a766af2d8b6798ce15ada9b
PMuiruri <peninah.m.muiruri@gmail.com>
2019-01-21 01:04:17 +0200

    updated app.js file and added the routes

--- h-project/src/App.js
+++ h-project/src/App.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import {Switch, Route} from 'react-router-dom';
+import Home from './components/home';
+import HomeworkList from './components/homework';
+import Student from './components/student';
+import Teacher from './components/teacher';
+import Login from './components/Login';
 import Navbar from './components/navbar';
 
 class App extends Component {
@@ -9,6 +15,14 @@ class App extends Component {
       <div className="App" >
         <header className="App-header">
 				<Navbar />
+				<Switch>
+				<Route exact path="/" component={Home} />
+				<Route path="/students" component={Student} />
+				<Route path="/parents" component={Home} />
+				<Route path="/teachers" component={Teacher} />
+				<Route path="/admin" component={HomeworkList} />
+				<Route path="/login" component={Login} />
+				</Switch>
         </header>
       </div>
     );
